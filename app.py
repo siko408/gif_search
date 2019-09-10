4bd1fa376d195b565b61a654980048ae0ae40064
John Miner <john.douglas.miner.iv@gmail.com>
2019-09-09 17:23:02 -0700

    Some stuff done.

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 def index():
     """Return homepage."""
     # TODO: Extract query term from url
-
+    # serch_term = request.args.get("search_term")
     # TODO: Make 'params' dict with query term and API key
-
+    params = {"api_key": "LIVDSRZULELA", "query": request.args.get("search_term")}
     # TODO: Make an API call to Tenor using the 'requests' library
-
+    request_gifs = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (params["api_key"], params["query"], 10))
     # TODO: Get the first 10 results from the search results
 
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
