8f988c9c38319b7928e89e42bb878dd303ab79c1
John Miner <john.douglas.miner.iv@gmail.com>
2019-09-10 17:13:46 -0700

    The gifs now appear and you can search them

--- app.py
+++ app.py
@@ -4,20 +4,32 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     """Return homepage."""
     # TODO: Extract query term from url
-    # serch_term = request.args.get("search_term")
+    search_term = "dog"
+    search_term = request.args.get("search_term")
     # TODO: Make 'params' dict with query term and API key
-    params = {"api_key": "LIVDSRZULELA", "query": request.args.get("search_term")}
+    params = {"api_key": "LIVDSRZULELA", "query": search_term}
     # TODO: Make an API call to Tenor using the 'requests' library
-    request_gifs = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (params["api_key"], params["query"], 10))
+    request_gifs = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (params["query"], params["api_key"], 10))
     # TODO: Get the first 10 results from the search results
 
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
+    # set the apikey and limit
+    gifid = ""
+    gifurl = ""
+    gifitemurl = ""
+    if request_gifs.status_code == 200:
+        gifs_json = json.loads(request_gifs.content)
+    else:
+        gifs_json = None
 
-    return render_template("index.html")
+    # return gifs_json
+    return render_template("index.html", gifs=gifs_json, gif_id=gifid, gif_url=gifurl, gif_itemurl=gifitemurl)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
